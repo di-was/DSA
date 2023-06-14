f15dbd1a86fab8bda902b120317b7e07ff42c519
Diwas Dahal <105503443+di-was@users.noreply.github.com>
2023-06-14 21:42:47 +0545

    Update cumulative_disk_space.py

--- recursive_algorithms/cumulative_disk_space.py
+++ recursive_algorithms/cumulative_disk_space.py
@@ -9,5 +9,3 @@ def cumulative_size(path):
             total += cumulative_size(childpath)
 
     return total
-
-print(cumulative_size("/home/diwas-dahal/Tm/Projects/Projects/javascript/cart/img/img"))
